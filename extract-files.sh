961f9feba3b2804fc1cbda0c9a83f962ad404b84
ToLiKNoSoV <toliknosov1994@yandex.ru>
2017-08-19 23:57:33 +0700

    Add files via upload

---
+++ extract-files.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+VENDOR=even
+DEVICE=g15v97
+BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+
+echo "Pulling $DEVICE files..."
+for FILE in `cat proprietary-blobs.txt | grep -v ^# | grep -v ^$`; do
+    DIR=`dirname $FILE`
+    if [ ! -d $BASE/$DIR ]; then
+        mkdir -p $BASE/$DIR
+    fi
+    adb pull /system/$FILE $BASE/$FILE
+done
+
+./setup-makefiles.sh
