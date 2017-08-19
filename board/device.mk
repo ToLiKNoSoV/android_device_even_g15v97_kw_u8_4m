47e0a936a7b19776688510e5efe857edb2222162
ToLiKNoSoV <toliknosov1994@yandex.ru>
2017-08-19 23:33:38 +0700

    Add files via upload

---
+++ board/device.mk
@@ -0,0 +1,10 @@
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := z,Z,umidigi,Umidigi,full_g15v97_kw_u8_4m,g15v97_kw_u8_4m,Z
+
+# Bionic
+
+# Enable Minikin text layout engine (will be the default soon)
+USE_MINIKIN := true
+
+# Configure jemalloc for low memory
+MALLOC_SVELTE := true
---
+++ board/telephony.mk
@@ -0,0 +1,3 @@
+# RIL
+BOARD_RIL_CLASS := ../../../device/even/g15v97_kw_u8_4m/ril
+BOARD_CONNECTIVITY_MODULE := conn_soc
